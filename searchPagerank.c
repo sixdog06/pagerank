f1db4a6324cec33b5ad38229fdbd19505c13a0d0
huanruiz <zhanghrharry@qq.com>
2019-01-27 16:19:53 +1100

    get num of urls and pagerank success

--- searchPagerank.c
+++ searchPagerank.c
@@ -7,45 +7,118 @@
 #include "readData.h"
 #include "InvertedIdx.h"
 
-void findMatchedUrls(FILE pr_file, char *word) {
+// find the macthed urls and insert to the double linked list
+void findMatchedUrls(char *pr_file, DLListStr L, char *word) {
+	char delim[2] = " ";
+	char *token;
+	char line[MAXSTRING];
+	FILE *f;
+	
+	if((f = fopen (pr_file , "r")) == NULL) {
+		return;
+	}
+	while(fgets(line, MAXSTRING, f) != NULL) {
+		int len = strlen(line);
+		if(line[len-1] == '\n') { // get rid of the '\n'
+			line[len-1] = '\0';
+		}
+		token = strtok(line, delim);
+		if(strcmp(token, word) == 0) {
+			while(token != NULL) {
+				if(strcmp(token, "\n") != 0 && strcmp(token, word) != 0) { // select the url and insert to the list
+					// printf("%s ", token);
+					insertSetOrd(L, token);
+				}
+				token = strtok(NULL, delim);
+			}		
+		}
+	}
+}
 
-	;
+void num_MatchedUrls(char *pr_file, DLListStr L, char *word, int *arr) {
+	char delim[2] = " ";
+	char *token;
+	char line[MAXSTRING];
+	FILE *f;
+	
+	if((f = fopen (pr_file , "r")) == NULL) {
+		return;
+	}
+	while(fgets(line, MAXSTRING, f) != NULL) {
+		int len = strlen(line);
+		if(line[len-1] == '\n') { // get rid of the '\n'
+			line[len-1] = '\0';
+		}
+		token = strtok(line, delim);
+		if(strcmp(token, word) == 0) {
+			while(token != NULL) {
+				if(strcmp(token, "\n") != 0 && strcmp(token, word) != 0) { // select the url and insert to the list
+					arr[show_Index(L, token)] += 1;
+				}
+				token = strtok(NULL, delim);
+			}		
+		}
+	}
+}
+
+void matched_Urls_with_PR(char *pr_file, DLListStr L, char *word, double *arr) {
+	char delim[2] = ",";
+	char *token;
+	char line[MAXSTRING];
+	FILE *f;
+	
+	if((f = fopen (pr_file , "r")) == NULL) {
+		return;
+	}
+	while(fgets(line, MAXSTRING, f) != NULL) {
+		int len = strlen(line);
+		if(line[len-1] == '\n') { // get rid of the '\n'
+			line[len-1] = '\0';
+		}
+		token = strtok(line, delim);
+		
+		if(strcmp(token, word) == 0) {
+			while(token != NULL) {
+				if(strcmp(token, "\n") != 0 && strcmp(token, word) != 0) { // select the url and insert to the list
+					// printf("%f!\n", atof(token));
+					arr[show_Index(L, word)] = atof(token);
+				}
+				token = strtok(NULL, delim);
+			}		
+		}		
+	}
 }
 
 int main(int argc, char *argv[]) {
-
-	double d = 0.85; //atof(argv[1]);
-	double diffPR = 0.00001; //atof(argv[2]);
-	double maxIteration = 1000; //atof(argv[3]);
-
-	// initialize double linked list to store the file names
-	DLListStr L = GetCollection();
-
-	// initialize the graph and store the url in it
-	Graph g = newGraph(L->nitems);
-	GetGraph(g, L);
-
-	// calculate the pagerank
-	Init_graph(g, L->nitems);
-	PageRank(g, L, d, diffPR, maxIteration);
-	Order(g, L);
-
-	FILE *fp;
-	fp = fopen("invertedIndex.txt", "w+");
-	L = GetCollection();
-	Tree t = newTree();
-	DLListNode *node = L->first;
+	int i;
+	DLListStr L = newDLListStr();
 	
-	while(node != NULL) {
-		t = readSection2(node->value, t);
+	// get the urls
+	for(i = 1; i < argc; i++) {
+		findMatchedUrls("invertedIndex.txt", L, argv[i]);
+	}
+	
+	// calculate the number of urls
+	int size = L->nitems;
+	int num[size];
+	for(i = 0; i < size; i++) {
+		num[i] = 0;
+	}
+	for(i = 1; i < argc; i++) {
+		num_MatchedUrls("invertedIndex.txt", L, argv[i], num);
+	}
+
+	// pagerank array initialize
+	double pr[size];
+	DLListNode *node = L->first;
+	for(i = 0; i < L->nitems; i++) {
+		matched_Urls_with_PR("pagerankList.txt", L, node->value, pr);
 		node = node->next;
 	}
-	// save tree to file
-	saveTree(t, fp);
-
-	free(t);
-	free(L);
-
-
+	// for(i = 0; i < L->nitems; i++) {
+	// 	printf("%.7f\n", pr[i]);
+	// }
+	
+	//showDLListStr(L);
 	return 0;
 }
